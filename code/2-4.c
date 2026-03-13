3d238161fee2112a33d6ef8b8ee6f478cf2aa716
chyunj2-adfs <chyunj2@gmail.com>
2026-03-13 10:22:02 +0900

    Add precision checks for floating-point equality

--- code/2-4.c
+++ code/2-4.c
@@ -2,7 +2,17 @@
 
 int main()
 {
+    float a = 0.1 + 0.2;
+    float b = 0.3;
+    printf("a = %.100f\n", a);
+    printf("b = %.100f\n", b);
+    printf("a == b is %d\n", a == b);
 
+    float c = 0.1 + 0.2;
+    float d = 0.3;
+    printf("c = %.100f\n", c);
+    printf("d = %.100f\n", d);
+    printf("c == d is %d\n", c == d);
     return 0;
 }
 
