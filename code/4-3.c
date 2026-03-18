f6175fffe72156e02f7ec238ad038e192483729f
chyunj2-adfs <chyunj2@gmail.com>
2026-03-18 21:26:06 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -4,14 +4,16 @@ int main()
 {
     int a;
     scanf("%d", &a);
-    for(int i = 2; i < a; i++)
-    {
-        if(a == 1){
-            printf("false");
-        }else if(a % i == 0){
-            printf("false");
-        }else{
-            printf("true");
+    if(a == 1){
+        printf("false");
+    }else{
+        for(int i = 2; i < a; i++)
+        {
+            if(a % i == 0){
+                printf("false");
+            }else{
+                printf("true");
+            }
         }
     }
     return 0;
