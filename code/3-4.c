25947b256096c75826e880c6ab6303487075f93d
chyunj2-adfs <chyunj2@gmail.com>
2026-03-18 21:05:32 +0900

    Implement variable assignments and arithmetic in main
    Added variable assignments and arithmetic operations with print statements.

--- code/3-4.c
+++ code/3-4.c
@@ -2,7 +2,14 @@
 
 int main()
 {
+    int a, b, c;
 
+    a = b = c = 5;//대입 연산은 오른쪽에서 왼쪽으로 진행한다
+    printf("%d %d %d\n", a, b, c);
+
+    a = 2 + 3 * 4;//곱셈이 덧셈보다 우선순위가 높다
+    printf("%d %d %d\n", a, b, c);
+    c = a++ + ++b;//++b를 먼저 사용하고, +를 실행하고, 나중에 a++한다.
+    printf("%d %d %d\n", a, b, c);
     return 0;
 }
-
