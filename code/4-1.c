b2e989abf995448891a51daa570002f7923a5c0b
chyunj2-adfs <chyunj2@gmail.com>
2026-03-18 21:10:52 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,19 @@
 
 int main()
 {
+    int a, b, sum;
+    char c;
 
+    scanf("%d %d %c", &a, &b, &c);
+    if(c == '+'){
+        sum = a + b;
+    } else if(c == '-'){
+        sum = a - b;
+    }else if (c == '*'){
+        sum = a * b;
+    }else {
+        sum = a / b;
+    }
+    printf("%d\n", sum);
     return 0;
 }
-
