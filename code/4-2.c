4eecc21793ecca595dd5cd107ab8f921567b6c89
chyunj2-adfs <chyunj2@gmail.com>
2026-03-18 21:23:36 +0900

    Change loop starting values from 0 to 1

--- code/4-2.c
+++ code/4-2.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    for(int i = 0; i < 10; i++) {
-        for(int j = 0; j < 10; j++) {
+    for(int i = 1; i < 10; i++) {
+        for(int j = 1; j < 10; j++) {
             printf("%d * %d = %d\n", i, j, i * j);
         }
     }
